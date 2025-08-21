09898c241f04a8b4136e83069dc6a8248da057de
Dhiraj <dkskp2005@gmail.com>
2025-08-21 11:50:43 +0530

    fixed

--- src/main/java/com/dhiraj/myapi_backend/security/JsonAuthEntryPoint.java
+++ src/main/java/com/dhiraj/myapi_backend/security/JsonAuthEntryPoint.java
@@ -8,19 +8,18 @@
 package com.dhiraj.myapi_backend.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+import jakarta.servlet.http.*;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import javax.security.sasl.AuthenticationException;
-import java.io.IOException;
-import java.util.Map;
-
 @Component
-public abstract class JsonAuthEntryPoint implements AuthenticationEntryPoint {
+public class JsonAuthEntryPoint implements AuthenticationEntryPoint {
     private final ObjectMapper mapper = new ObjectMapper(); // JSON mapper. //
 
+    @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
         response.setStatus(401); // 401 Unauthorized. //
         response.setContentType("application/json"); // JSON. //
